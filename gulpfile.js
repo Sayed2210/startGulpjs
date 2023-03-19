2f38d24dba74d0e80b4ee885af68a8afc1ff2116
Elsayed Hassan <eh600039@gmail.com>
2023-03-19 17:19:38 +0200

    done

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
 const gulp = require('gulp'),
-concat = require('gulp-concat'),
-autoprefixer = require('gulp-autoprefixer'),
-sass = require('gulp-sass')(require('sass'));
+    concat = require('gulp-concat'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass')(require('sass')),
+    livereload = require('gulp-livereload'),
+    pug = require('gulp-pug'),
+    watch = require('gulp-watch');
 
+gulp.task('pug',function(){
+        return gulp.src('stage/index.pug')
+                .pipe(pug({pretty: true}))
+                .pipe(gulp.dest('dist'))
+                .pipe(livereload())
+    })
 gulp.task('scss',function(){
-    return gulp.src('stage/css/homepage.scss')
+    return gulp.src('stage/css/**/*.*')
             .pipe(sass())
-            .pipe(autoprefixer())
             .pipe(concat('style.css'))
+            .pipe(autoprefixer())
             .pipe(gulp.dest('dist'))
+            .pipe(livereload())
 })
+gulp.task('stream', function () {
+    // Endless stream mode
+    livereload.listen();
+    gulp.watch('stage/css/homepage.scss', gulp.series('scss'));
+    gulp.watch('stage/index.pug', gulp.series('pug'))
+});
